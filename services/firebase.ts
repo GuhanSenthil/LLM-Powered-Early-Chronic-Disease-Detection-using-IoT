f6d415382d7a6813779730234c5253220d3a544f
Guhan S <guhansenthil1234@gmail.com>
2026-04-29 16:01:09 +0530

    Clear Firebase configuration values
    Removed sensitive Firebase configuration details.

--- services/firebase.ts
+++ services/firebase.ts
@@ -3,14 +3,14 @@ import 'firebase/compat/database';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA1jt4keHEDiu5pQE2ML3dxBPRGk6uTwls",
-  authDomain: "final-f9b7e.firebaseapp.com",
-  databaseURL: "https://final-f9b7e-default-rtdb.firebaseio.com",
-  projectId: "final-f9b7e",
-  storageBucket: "final-f9b7e.appspot.com",
-  messagingSenderId: "224390321920",
-  appId: "1:224390321920:web:2ce1c493620bd0efb045e5",
-  measurementId: "G-75XGLPLBHZ"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Fix: Use Firebase v8 namespaced API for initialization to resolve export errors.
